93e8c4e06aa4079dbdc04d365656bfae0149266f
abbiyanaila <abbiyanaila@gmail.com>
2019-04-16 00:50:55 +0800

    feat: add sintaks for username

--- create_database.py
+++ create_database.py
@@ -1,10 +1,15 @@
 import sqlite3
+import config
+import os
 
-connection = sqlite3.connect('../../db/data.db')
+db_path = os.path.join(config.BASE_DIR, 'db', 'data.db')
+connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
+cursor.execute("INSERT INTO users VALUES (1, 'desi', 'qwerty')")
+
 
 create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
 cursor.execute(create_table)
