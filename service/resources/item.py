4a99430b47b6f7a6f2671d7d1b7e230a4c1f56b4
abbiyanaila <abbiyanaila@gmail.com>
2019-04-20 20:14:12 +0800

    feat: add itemmodel

--- service/resources/item.py
+++ service/resources/item.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from service.common import connector
+from service.models.item_model import ItemModel
 
 class Item(Resource): #every resource has to be a class
     TABLE_NAME = 'items'
@@ -14,27 +15,14 @@ class Item(Resource): #every resource has to be a class
 
     @jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
             return item
         return {'massage': 'Item not found'}, 404
 
-    @classmethod
-    def find_by_name(cls, name):
-        connection = connector.get_connection()
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
-
 
     def post(self, name):
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'massage': "An item with name '{}' already exists.".format(name)}
 
         data = Item.parser.parse_args()
@@ -42,20 +30,12 @@ class Item(Resource): #every resource has to be a class
         item = {'name': name, 'price': data['price']}
 
         try:
-            Item.insert(item)
+            ItemModel.insert(item)
         except:
             return {'massage': "An error inserting the item"}
 
         return item #information for data was create or not
 
-
-    @classmethod
-    def insert(cls, item):
-        connection = connector.get_connection()
-        cursor = connection.cursor()
-
-        query = "INSERT INTO {table} VALUES(?, ?)".format(table=cls.TABLE_NAME)
-        cursor.execute(query, (item['name'], item['price']))
 
     @jwt_required()
     def delete(self, name):
@@ -70,28 +50,21 @@ class Item(Resource): #every resource has to be a class
     @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
             try:
-                Item.insert(updated_item)
+                ItemModel.insert(updated_item)
             except:
                 return {"message": "An error occurred inserting the item."}
         else:
             try:
-                Item.update(updated_item)
+                ItemModel.update(updated_item)
             except:
                 raise
                 return {"message": "An error occurred updating the item."}
         return updated_item
-
-    @classmethod
-    def update(cls, item):
-        connection = connector.get_connection()
-        cursor = connection.cursor()
-
-        query = "UPDATE {table} SET price=? WHERE name=?".format(table=cls.TABLE_NAME)
-        cursor.execute(query, (item['price'], item['name']))
 
 class ItemList(Resource):
     TABLE_NAME = 'items'
