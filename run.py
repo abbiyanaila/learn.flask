155069a07183bb279eeb18183672c43ecca3483b
abbiyanaila <abbiyanaila@gmail.com>
2019-04-20 17:55:49 +0800

    feat: import db

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-import config
 from service.app import *
 # print(config.BASE_DIR)
 
+if __name__ == '__main__':
+    from db_sqlalchemy import db
+    db.init_app(app)
+    app.run(port=8401, debug=True)
 
-app.run(port=8401, debug=True)
-#
+
+
 
