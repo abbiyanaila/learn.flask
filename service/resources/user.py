b1fe1564ff5b700d315c665a98551b7b18b8c6e6
abbiyanaila <abbiyanaila@gmail.com>
2019-04-20 18:40:25 +0800

    feat: add UserModel

--- service/resources/user.py
+++ service/resources/user.py
@@ -1,44 +1,7 @@
 from flask_restful import Resource, reqparse
 from service.common import connector
+from service.models.user_model import UserModel
 
-class User():
-
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def find_by_username(cls, username):
-        connection = connector.get_connection()
-        cursor = connector.get_cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
-
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = connector.get_connection()
-        cursor = connector.get_cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
 
 class UserRegister(Resource):
 
@@ -57,7 +20,7 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"massage": "A user witth hat usernamse already exist"}, 400
 
         connection = connector.get_connection()
