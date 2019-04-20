5a253d6dc8173ff833cd3c72b5f1d689b0644162
abbiyanaila <abbiyanaila@gmail.com>
2019-04-20 18:44:11 +0800

    feat: add usermodel

--- service/common/security.py
+++ service/common/security.py
@@ -1,13 +1,13 @@
 from werkzeug.security import safe_str_cmp
-from service.resources import user
+from service.models.user_model import UserModel
 
 
 def authenticate(username, password):
-    usr = user.User.find_by_username(username)
+    usr = UserModel.find_by_username(username)
     if user and safe_str_cmp(usr.password, password):
         return usr
 
 
 def identity(payload):
     user_id = payload['identity']
-    return user.User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
