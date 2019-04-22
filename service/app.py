b5404be773a0bbea0f6880a791114a63b55c5f4b
abbiyanaila <abbiyanaila@gmail.com>
2019-04-22 19:55:16 +0800

    feat: add app.config

--- service/app.py
+++ service/app.py
@@ -2,12 +2,12 @@ from flask  import Flask
 from flask_restful import Api
 from service.common import security
 from flask_jwt import JWT
-from service.resources.user import User
+from service.resources.item import Item
 from service.resources.item import ItemList
 from service.resources.user import UserRegister
 
-
 app = Flask(__name__) #special python variable, unique name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'qwerty'
 api = Api(app)
 
@@ -15,7 +15,7 @@ jwt = JWT(app, security.authenticate, security.identity)
 # /aut   h , we send username and password, and JWT extension gets that username
 # and password
 
-api.add_resource(item.Item, '/item/<string:name>') #pengganti route pada class Item
+api.add_resource(Item, '/item/<string:name>') #pengganti route pada class Item
 api.add_resource(ItemList, '/items') #pengganti route pada class ItemList
 api.add_resource(UserRegister, '/register' )
 
