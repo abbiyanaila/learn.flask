19954e7934632d7a0599d59fc1ed67cd6f0b67e2
abbiyanaila <abbiyanaila@gmail.com>
2019-04-30 16:40:50 +0800

    feat: add sintaks for user_model

--- service/models/user_model.py
+++ service/models/user_model.py
@@ -1,7 +1,5 @@
-from service.common import connector
 from database import Base, db_sess
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import mapper
 
 class UserModel(Base):
     __tablename__ = 'users'
@@ -9,55 +7,31 @@ class UserModel(Base):
     username = Column(String(80))
     password = Column(String(80))
 
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
     def __repr__(self):
-        return '<UserRegister %r>' % (self.username)
+        return '<User %r>' % (self.username)
 
 class UserDAO(object):
 
     @staticmethod
-    def save(item):
-        db_sess.add(item)
+    def save(user: UserModel):
+        db_sess.add(user)
         db_sess.commit()
 
     @staticmethod
-    def find_by_username(cls, username):
-        query = db_sess.query(UserModel).filter_by(name=username)
+    def find_by_username(username):
+        query = db_sess.query(UserModel).filter_by(username=username)
         res = list(query)
         if len(res)==0:
-            return pass
+            return False
         else:
             return res
-        # connection = connector.get_connection()
-        # cursor = connector.get_cursor()
-        #
-        # query = "SELECT * FROM users WHERE username=?"
-        # result = cursor.execute(query, (username,))
-        # row = result.fetchone()
-        # if row:
-        #     user = cls(*row)
-        # else:
-        #     user = None
-        #
-        # connection.close()
-        # return user
 
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = connector.get_connection()
-        cursor = connector.get_cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
+    @staticmethod
+    def find_by_id(id):
+        query = db_sess.query(UserModel).filter_by(id=id)
+        res = list(query)
+        if len(res)==0:
+            return False
         else:
-            user = None
+            return res
 
-        connection.close()
-        return user
