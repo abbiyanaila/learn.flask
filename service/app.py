703bfe14636b3ad312ae350f69aee316df4a1f4e
abbiyanaila <abbiyanaila@gmail.com>
2019-05-02 21:52:21 +0800

    feat: add api resource store

--- service/app.py
+++ service/app.py
@@ -5,18 +5,23 @@ from flask_jwt import JWT
 from service.resources.item import Item
 from service.resources.item import ItemList
 from service.resources.user import UserRegister
+from flask_sqlalchemy import SQLAlchemy
+from service.resources.store import Store, StoreList
 
 app = Flask(__name__) #special python variable, unique name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'qwerty'
+db = SQLAlchemy(app)
 api = Api(app)
 
 jwt = JWT(app, security.authenticate, security.identity)
 # /aut   h , we send username and password, and JWT extension gets that username
 # and password
 
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') #pengganti route pada class Item
 api.add_resource(ItemList, '/items') #pengganti route pada class ItemList
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register' )
 
