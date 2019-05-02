c3238855c4dc2afc774ebfe70ac36137eb228186
abbiyanaila <abbiyanaila@gmail.com>
2019-05-02 13:58:56 +0800

    feat: add some function in store model

---
+++ service/models/store_model.py
@@ -0,0 +1,38 @@
+from database import Base, db_sess
+from sqlalchemy import Column, Integer, String
+
+class StoreModel(Base):
+    __tablename__ = 'stores'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {'name': self.name, 'items': self.items}
+
+class storeDAO(object):
+
+    @staticmethod
+    def save(store: StoreModel):
+        db_sess.add(store)
+        db_sess.commit()
+
+    @staticmethod
+    def find_by_name(name):
+        query = db_sess.query(StoreModel).filter_by(name=name)
+        res = list(query)
+        if len(res)==0:
+            return False
+        else:
+            return res
+
+    @staticmethod
+    def delete(name):
+        res = storeDAO.find_by_name(name=name)
+        if res:
+            db_sess.delete(res[0])
+
+
