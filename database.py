921970eab636343f917cac368bd5c7d60627328b
abbiyanaila <abbiyanaila@gmail.com>
2019-05-13 14:57:10 +0800

    feat: try to fix the sintaks

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 import config
 import os
